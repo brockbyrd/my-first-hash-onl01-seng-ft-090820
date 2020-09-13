cb83602329d3dbf3aa6da686e7756976ddb5f05b
Brock Byrd <brockbyrdd@gmail.com>
2020-09-13 21:55:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  your_hash = { "name" => "Brock" }
   
 end
 
@@ -21,7 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest = ["oil paintings"]
 end
 
 def adding
